85741132de25786b78da1bfc258265357a120083
Eric Camargo <ericbobcamargo@gmail.com>
2020-11-25 21:20:06 -0300

    Randomization of images and creation of gitignore

--- randomizing-img.js
+++ randomizing-img.js
@@ -1,5 +1,5 @@
 
-var images = ['Astronauts.png', 'Squad-HinodeTrio.png', 'Matrix.png', 'Ninjas.png', 'PartyTrio.png', 'Star-Wars.png', 'Super-Heroes.png'];
+var images = ['Astronauts.png', 'hinode.png', 'matrix.png', 'ninjas.png', 'party.png', 'starwars.png', 'superheroes.png'];
 var size = images.length
 var x = Math.floor(Math.random() * size)
 console.log(images[x])
