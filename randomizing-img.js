a0c408caa4a74a7fc4c8e2dd83488af891770a62
Eric Camargo <ericbobcamargo@gmail.com>
2020-11-25 21:44:03 -0300

    Fixed minor bugs with files names

--- randomizing-img.js
+++ randomizing-img.js
@@ -1,5 +1,5 @@
 
-var images = ['new-astronauts.png', 'hinode.png', 'matrix.png', 'ninjas.png', 'party.png', 'starwars.png', 'superheroes.png'];
+var images = ['new-astronauts.png', 'hinode.png', 'new-matrix.png', 'new-ninjas.png', 'party.png', 'starwars.png', 'superheroes.png'];
 var size = images.length
 var x = Math.floor(Math.random() * size)
 console.log(images[x])
